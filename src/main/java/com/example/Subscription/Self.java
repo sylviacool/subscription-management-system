2d2596501ce4de1598efcdb560d48b665cd06b79
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-03-18 20:23:51 +0100

    feat: implement Self and Spouse user classes

--- src/main/java/com/example/Subscription/Self.java
+++ src/main/java/com/example/Subscription/Self.java
@@ -1,12 +1,26 @@
 package com.example.Subscription;
 
-// Override the User Interface methods in Self class
-public class Self{
+
+public class Self implements User{
 
     private String name;
     private String memberName = "self";
     private int age;
     private Long contact;
     private String address;
+    
+	@Override
+	public void setUserDetails(String name, int age, Long contact, String address) {
+		this.name = name;
+		this.age = age;
+		this.contact = contact;
+		this.address = address;
+		
+	}
+	@Override
+	public void getUserDetails() {
+		System.out.println(this.name + memberName + this.age + this.contact + this.address);
+		
+	}
 
 }
--- src/main/java/com/example/Subscription/Spouse.java
+++ src/main/java/com/example/Subscription/Spouse.java
@@ -1,11 +1,26 @@
 package com.example.Subscription;
 
 // Override the User Interface methods in Spouse class
-public class Spouse{
+public class Spouse implements User{
     private String name;
     private String memberName = "spouse";
     private int age;
     private Long contact;
     private String address;
+    
+    @Override
+	public void setUserDetails(String name, int age, Long contact, String address) {
+		this.name = name;
+		this.age = age;
+		this.contact = contact;
+		this.address = address;
+		
+	}
+	@Override
+	public void getUserDetails() {
+		System.out.println(this.name + memberName + this.age + this.contact + this.address);
+		
+	}
+
 
 }
